9765af23d7c44a57e4cf94f475354b21a526d797
shadymohamedamin <60430920+shadymohamedamin@users.noreply.github.com>
2021-05-09 06:49:22 +0200

    Update quick_sort_my.cpp

--- quick_sort_my.cpp
+++ quick_sort_my.cpp
@@ -39,3 +39,52 @@ int main()
     for(int i=0;i<v.size();i++)
         cout <<v[i] <<" ";
 }
+
+
+
+
+
+/*#include <iostream>
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+ll partition(vector<ll>&arr,ll l,ll r)
+{
+    ll pivot=arr[l];
+    ll pivot_index=l;
+    for(ll i=l+1;i<=r;i++)
+    {
+        if(arr[i]<=pivot)
+        {
+            pivot_index++;
+            swap(arr[i],arr[pivot_index]);
+           // pivot_index++;
+        }
+    }
+    swap(arr[l],arr[pivot_index]);
+    return pivot_index;
+}
+
+void quick(vector<ll>&arr,ll l,ll r)
+{
+    if(l<r)
+    {
+        ll index=partition(arr,l,r);
+        quick(arr,index+1,r);
+        quick(arr,l,index-1);
+    }
+}
+int main()
+{
+    ll n;
+    cin>>n;
+    vector<ll>arr(n,0);
+    for(ll i=0;i<n;i++)
+    cin>>arr[i];
+    quick(arr,0,n-1);
+    for(ll i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+    //cout<<"Hello World";
+
+    return 0;
+}*/
